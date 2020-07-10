aad1658fd9833af9b609707a0230ea554494e317
Lakshay Sharma <laksh1013shar@gmail.com>
2020-07-10 22:05:14 +0530

    Updates

---
+++ precache-manifest.ef9144f5ff26f3251779a145220d5292.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0586b2dd3524a81c31cf71d8451918d",
+    "url": "/sharmaLakshay/index.html"
+  },
+  {
+    "revision": "769f7345b5c131bb7518",
+    "url": "/sharmaLakshay/static/css/3.01c87591.chunk.css"
+  },
+  {
+    "revision": "0138e22cb1595fad78b8",
+    "url": "/sharmaLakshay/static/css/main.1d09cc41.chunk.css"
+  },
+  {
+    "revision": "c2265b26cd958c259c26",
+    "url": "/sharmaLakshay/static/js/2.c54b274e.chunk.js"
+  },
+  {
+    "revision": "769f7345b5c131bb7518",
+    "url": "/sharmaLakshay/static/js/3.5f044ac5.chunk.js"
+  },
+  {
+    "revision": "0138e22cb1595fad78b8",
+    "url": "/sharmaLakshay/static/js/main.34cccd9e.chunk.js"
+  },
+  {
+    "revision": "da4baea176011865c2ca",
+    "url": "/sharmaLakshay/static/js/runtime-main.60efa92d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/sharmaLakshay/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "68feb875a3eb7d8d45e81c9288e2f7bf",
+    "url": "/sharmaLakshay/static/media/Bank.68feb875.gif"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/sharmaLakshay/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3512ce3648f49829ccff789cd97181a0",
+    "url": "/sharmaLakshay/static/media/Visitor.3512ce36.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/sharmaLakshay/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/sharmaLakshay/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "edb0085ebeb8a5ab17d1425b18416ecb",
+    "url": "/sharmaLakshay/static/media/fcc.edb0085e.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sharmaLakshay/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/sharmaLakshay/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/sharmaLakshay/static/media/skill.34409398.svg"
+  }
+]);
